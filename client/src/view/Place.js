75139f6e7140890697b6c70619a6c048d822324f
mateuszbuturla <2002mateuszbuturla@gmail.com>
2020-01-27 09:55:31 +0100

    create Place page

--- client/src/view/Place.js
+++ client/src/view/Place.js
@@ -3,24 +3,35 @@ import React from 'react';
 class Place extends React.Component {
 
     state = {
-        place: {}
+        place: {},
+        err: false,
     }
 
     componentDidMount() {
         const name = this.props.match.params.name;
         fetch(`http://localhost:4000/api/getmainplacebyname/${name}`, { method: 'POST' })
             .then(r => r.json())
-            .then(r => this.setState({ place: r[0] }))
+            .then(r => {
+                if (r.length === 0)
+                    this.setState({ err: true })
+                else
+                    this.setState({ place: r[0] })
+            })
     }
 
     render() {
-        const { place } = this.state;
+        const { place, err } = this.state;
         return (
             <section>
                 {place.name !== '' &&
                     <div>
                         <h2>{place.name}</h2>
                         <p>{place.description}</p>
+                    </div>
+                }
+                {err === true &&
+                    <div>
+                        <h2>404</h2>
                     </div>
                 }
             </section>
