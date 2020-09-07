ed22fffd881db8f50febe4933c4c80eab3b6137b
mateuszbuturla <2002mateuszbuturla@gmail.com>
2020-09-07 20:04:09 +0200

    resolve conflict

--- client/src/components/layout/Footer/index.tsx
+++ client/src/components/layout/Footer/index.tsx
@@ -25,51 +25,6 @@ function Footer() {
 
   return (
     <StyledFooter>
-<<<<<<< Updated upstream
-      <StyledFooterContainer>
-        <StyledFooterList>
-          <StyledFooterListElement header>Regiony</StyledFooterListElement>
-          <StyledFooterListElement>Hokkaido</StyledFooterListElement>
-          <StyledFooterListElement>Tohoku</StyledFooterListElement>
-          <StyledFooterListElement>Kanto</StyledFooterListElement>
-          <StyledFooterListElement>Chubu</StyledFooterListElement>
-          <StyledFooterListElement>Kansai</StyledFooterListElement>
-          <StyledFooterListElement>Chugoku</StyledFooterListElement>
-          <StyledFooterListElement>Shikoku</StyledFooterListElement>
-          <StyledFooterListElement>Kyushu</StyledFooterListElement>
-          <StyledFooterListElement>Okinawa</StyledFooterListElement>
-        </StyledFooterList>
-        <StyledFooterList>
-          <StyledFooterListElement header>
-            Topowe lokacje
-          </StyledFooterListElement>
-          <StyledFooterListElement>Tokyo</StyledFooterListElement>
-          <StyledFooterListElement>Kyoto</StyledFooterListElement>
-          <StyledFooterListElement>Osaka</StyledFooterListElement>
-          <StyledFooterListElement>Nara</StyledFooterListElement>
-          <StyledFooterListElement>Góra Fuji</StyledFooterListElement>
-          <StyledFooterListElement>Hiroshima</StyledFooterListElement>
-          <StyledFooterListElement>Kamakura</StyledFooterListElement>
-        </StyledFooterList>
-        <StyledFooterList>
-          <StyledFooterListElement header>Atrakcje</StyledFooterListElement>
-          <StyledFooterListElement>Świątynie</StyledFooterListElement>
-          <StyledFooterListElement>Shrines</StyledFooterListElement>
-          <StyledFooterListElement>Onses</StyledFooterListElement>
-          <StyledFooterListElement>Ogrody</StyledFooterListElement>
-        </StyledFooterList>
-        <StyledFooterList>
-          <StyledFooterListElement header>Kuchnia</StyledFooterListElement>
-          <StyledFooterListElement>Zupy</StyledFooterListElement>
-          <StyledFooterListElement>Sushi</StyledFooterListElement>
-          <StyledFooterListElement>Takoyaki</StyledFooterListElement>
-          <StyledFooterListElement>Yakitori</StyledFooterListElement>
-          <StyledFooterListElement>Tonkatsu</StyledFooterListElement>
-          <StyledFooterListElement>Dango</StyledFooterListElement>
-          <StyledFooterListElement>Onigiri</StyledFooterListElement>
-        </StyledFooterList>
-      </StyledFooterContainer>
-=======
       {footerData && (
         <StyledFooterContainer>
           {footerData.map((category: any) => (
@@ -88,7 +43,6 @@ function Footer() {
           ))}
         </StyledFooterContainer>
       )}
->>>>>>> Stashed changes
       <StyledFooterCopyright>
         Copyright © 2020-2020 Japan Tour Mateusz Buturla All Rights Reserved.
       </StyledFooterCopyright>
