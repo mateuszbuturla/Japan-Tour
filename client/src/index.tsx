d0649643c9590c84e3217f7b9da1ba06e0262680
mateuszbuturla <2002mateuszbuturla@gmail.com>
2020-09-08 10:16:08 +0200

    change pagr title

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,22 +2,16 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { Helmet } from 'react-helmet';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import store from './store';
 import Theme from './config/styledGlobal';
+import PageHelmet from './config/PageHelmet';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <Helmet>
-        <title></title>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
-          rel="stylesheet"
-        />
-      </Helmet>
+      <PageHelmet />
       <Theme>
         <BrowserRouter>
           <App />
