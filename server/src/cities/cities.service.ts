a38b9ca6eba9b3c21f64a1f6a3337d07b8d1cc8a
mateuszbuturla <2002mateuszbuturla@gmail.com>
2020-09-28 11:48:49 +0200

    create city endpoint

--- server/src/cities/cities.service.ts
+++ server/src/cities/cities.service.ts
@@ -1,6 +1,8 @@
-import { Injectable, NotFoundException } from "@nestjs/common";
+import { HttpException, Injectable, NotFoundException } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
+import { isNull } from "util";
+import NormalizeString from "../utils/normalizeString";
 
 import { City } from "./city.model";
 
@@ -37,5 +39,35 @@ export class CitiesService {
       throw new NotFoundException("Could not find city.");
     }
     return city;
+  }
+
+  async createCity(data: City) {
+    let res;
+    const existCity = await this.cityModel
+      .findOne({
+        $or: [
+          { name: data.name },
+          { url: NormalizeString(data.name) },
+          { key: NormalizeString(data.name) },
+        ],
+      })
+      .exec();
+
+    if (isNull(existCity)) {
+      const newCity = new this.cityModel({
+        name: data.name,
+        url: NormalizeString(data.name),
+        key: NormalizeString(data.name),
+        region: data.region,
+        description: data.description,
+        img: data.img,
+        otherData: data.otherData,
+      });
+      res = await newCity.save();
+    } else {
+      throw new HttpException("City is exist.", 409);
+    }
+
+    return res;
   }
 }
