10db5bd67bec636f1ff12cd2ede056f3984a209c
mateuszbuturla <2002mateuszbuturla@gmail.com>
2020-09-29 13:26:59 +0200

    create category endpoint

--- server/src/categories/categories.controller.ts
+++ server/src/categories/categories.controller.ts
@@ -1,10 +1,16 @@
-import { Controller, Body, Get, Param } from "@nestjs/common";
+import { Controller, Body, Get, Param, Post } from "@nestjs/common";
 
 import { CategoriesService } from "./categories.service";
+import { Category } from "./category.model";
 
 @Controller("/api/categories")
 export class CategoriesController {
   constructor(private readonly categoriesService: CategoriesService) {}
+
+  @Post("create")
+  createCategory(@Body() data: Category) {
+    return this.categoriesService.createCategory(data);
+  }
 
   @Get(":section")
   async getAllCategories(@Param("section") section: string) {
