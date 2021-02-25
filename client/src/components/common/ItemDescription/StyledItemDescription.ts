08c1a959d0188aa35bc8f154f4d45a1876f59d08
mateuszbuturla <2002mateuszbuturla@gmail.com>
2021-02-25 15:28:27 +0100

    fix item description image size

---
+++ client/src/components/common/ItemDescription/StyledItemDescription.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const StyledItemDescription = styled.div`
+  width: 100%;
+
+  img {
+    width: 100%;
+  }
+`;
+
+export { StyledItemDescription };
--- client/src/components/common/ItemDescription/index.tsx
+++ client/src/components/common/ItemDescription/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactHtmlParser from 'react-html-parser';
+import { StyledItemDescription } from './StyledItemDescription';
 
 interface Props {
   description: string;
 }
 
 function ItemDescription({ description }: Props) {
-  return <div>{ReactHtmlParser(description)}</div>;
+  return <StyledItemDescription>{ReactHtmlParser(description)}</StyledItemDescription>;
 }
 
 export default ItemDescription;
