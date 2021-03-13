5debd9ace2feb1f3cd1ce92351f8142f64e228e4
mateuszbuturla <2002mateuszbuturla@gmail.com>
2021-03-13 16:09:15 +0100

    back-end get single prefecture endpoint

--- server/src/prefecture/prefecture.controller.ts
+++ server/src/prefecture/prefecture.controller.ts
@@ -1,5 +1,5 @@
-import { Controller, Get } from '@nestjs/common';
-import { RegionInterface } from 'src/interfaces/region';
+import { Controller, Get, Param } from '@nestjs/common';
+import { PrefectureInterface } from 'src/interfaces/prefecture';
 import { PrefectureService } from './prefecture.service';
 
 @Controller('prefecture')
@@ -7,8 +7,16 @@ export class PrefectureController {
   constructor(private readonly prefectureService: PrefectureService) {}
 
   @Get('/')
-  async getPrefectures(): Promise<RegionInterface[]> {
+  async getPrefectures(): Promise<PrefectureInterface[]> {
     const prefectures = await this.prefectureService.getPrefectures();
     return prefectures;
+  }
+
+  @Get('/:key')
+  async getSinglePrefecture(
+    @Param('key') key: string,
+  ): Promise<PrefectureInterface> {
+    const region = await this.prefectureService.getSinglePrefecture(key);
+    return region;
   }
 }
--- server/src/prefecture/prefecture.service.ts
+++ server/src/prefecture/prefecture.service.ts
@@ -1,5 +1,5 @@
 import { PrefectureInterface } from './../interfaces/prefecture';
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Prefecture } from './prefecture.entity';
@@ -23,5 +23,24 @@ export class PrefectureService {
       region: prefecture.region,
       highlight: prefecture.highlight,
     }));
+  }
+
+  async getSinglePrefecture(key: string): Promise<PrefectureInterface> {
+    const prefecture = await this.prefectureModel.findOne({ key });
+
+    if (!prefecture) {
+      throw new NotFoundException();
+    }
+
+    return {
+      id: prefecture._id,
+      name: prefecture.name,
+      key: prefecture.key,
+      shortDescription: prefecture.shortDescription,
+      description: prefecture.description,
+      img: prefecture.img,
+      region: prefecture.region,
+      highlight: prefecture.highlight,
+    };
   }
 }
