134cc5f5a0050832605079a5116a616f084ae931
mateuszbuturla <2002mateuszbuturla@gmail.com>
2021-03-13 16:53:09 +0100

    back-end create prefecture endpoint

--- server/src/prefecture/prefecture.controller.ts
+++ server/src/prefecture/prefecture.controller.ts
@@ -1,6 +1,21 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  UploadedFiles,
+  UseInterceptors,
+  UsePipes,
+} from '@nestjs/common';
+import { FileFieldsInterceptor } from '@nestjs/platform-express';
+import { MulterDiskUploadedFiles } from 'src/interfaces/files';
 import { PrefectureInterface } from 'src/interfaces/prefecture';
+import { JoiValidationPipe } from 'src/pipes/JoiValidationPipe';
+import { multerStorage } from 'src/utils/storage';
+import AddPrefectureDto from './dto/AddPrefectureDto';
 import { PrefectureService } from './prefecture.service';
+import { AddPrefectureValidator } from './validation/AddPrefectureValidator';
 
 @Controller('prefecture')
 export class PrefectureController {
@@ -20,6 +35,29 @@ export class PrefectureController {
       key,
     );
     return prefectures;
+  }
+
+  @Post('/create')
+  @UsePipes(new JoiValidationPipe(AddPrefectureValidator))
+  @UseInterceptors(
+    FileFieldsInterceptor(
+      [
+        {
+          name: 'img',
+          maxCount: 1,
+        },
+      ],
+      {
+        storage: multerStorage(),
+      },
+    ),
+  )
+  async createRegion(
+    @Body() data: AddPrefectureDto,
+    @UploadedFiles() img: MulterDiskUploadedFiles,
+  ) {
+    const prefecture = await this.prefectureService.createPrefecture(data, img);
+    return prefecture;
   }
 
   @Get('/:key')
--- server/src/prefecture/prefecture.service.ts
+++ server/src/prefecture/prefecture.service.ts
@@ -1,6 +1,7 @@
 import { PrefectureInterface } from './../interfaces/prefecture';
 import {
   forwardRef,
+  HttpException,
   Inject,
   Injectable,
   NotFoundException,
@@ -9,6 +10,12 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Prefecture } from './prefecture.entity';
 import { RegionService } from 'src/region/region.service';
+import AddPrefectureDto from './dto/AddPrefectureDto';
+import { MulterDiskUploadedFiles } from 'src/interfaces/files';
+import NormalizeString from 'src/utils/normalizeString';
+import { storageDir } from 'src/utils/storage';
+import * as fs from 'fs';
+import * as path from 'path';
 
 @Injectable()
 export class PrefectureService {
@@ -66,5 +73,53 @@ export class PrefectureService {
       region: prefecture.region,
       highlight: prefecture.highlight,
     }));
+  }
+
+  async createPrefecture(
+    data: AddPrefectureDto,
+    imgs: MulterDiskUploadedFiles,
+  ) {
+    const img = imgs?.img?.[0] ?? null;
+
+    if (!img) {
+      throw new HttpException('Validation failed', 400);
+    }
+
+    try {
+      const region = await this.regionService.getSingleRegions(data.region);
+
+      if (!region) {
+        throw new HttpException('Validation failed', 400);
+      }
+
+      const existPrefecture = await this.prefectureModel
+        .find({
+          $or: [{ name: data.name }, { key: NormalizeString(data.name) }],
+        })
+        .exec();
+      if (existPrefecture.length > 0) {
+        throw new HttpException('Prefecture is exist.', 409);
+      } else {
+        const newPrefecture = new this.prefectureModel({
+          name: data.name,
+          key: NormalizeString(data.name),
+          img: img.filename,
+          description: data.description,
+          shortDescription: data.shortDescription,
+          region: region.id,
+          highlight: data.highlight,
+        });
+        const res = await newPrefecture.save();
+        return res;
+      }
+    } catch (e: any) {
+      try {
+        if (img) {
+          fs.unlinkSync(path.join(storageDir(), img.filename));
+        }
+      } catch (e2) {}
+
+      throw e;
+    }
   }
 }
