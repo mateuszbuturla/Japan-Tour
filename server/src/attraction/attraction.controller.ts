db04dd546fa5f003985aa3e00e668b02a18e5650
mateuszbuturla <2002mateuszbuturla@gmail.com>
2021-03-14 17:09:06 +0100

    back-end get single attractions endpoint

--- server/src/attraction/attraction.controller.ts
+++ server/src/attraction/attraction.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { AttractionInterface } from 'src/interfaces/attraction';
 import { AttractionService } from './attraction.service';
 
@@ -10,5 +10,13 @@ export class AttractionController {
   async getCities(): Promise<AttractionInterface[]> {
     const attractions = await this.attractionService.getAttractions();
     return attractions;
+  }
+
+  @Get('/:key')
+  async getSingleAttraction(
+    @Param('key') key: string,
+  ): Promise<AttractionInterface> {
+    const attraction = await this.attractionService.getSingleAttraction(key);
+    return attraction;
   }
 }
--- server/src/attraction/attraction.service.ts
+++ server/src/attraction/attraction.service.ts
@@ -1,5 +1,5 @@
 import { Attraction } from './attraction.entity';
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { AttractionInterface } from 'src/interfaces/attraction';
@@ -25,5 +25,24 @@ export class AttractionService {
       city: attraction.city,
       highlight: attraction.highlight,
     }));
+  }
+
+  async getSingleAttraction(key: string): Promise<AttractionInterface> {
+    const attraction = await this.findAttraction(key);
+
+    return attraction;
+  }
+
+  private async findAttraction(key: string): Promise<AttractionInterface> {
+    let attraction;
+    try {
+      attraction = await this.attractionModel.findOne({ key }).exec();
+    } catch (error) {
+      throw new NotFoundException('Could not find attraction.');
+    }
+    if (!attraction) {
+      throw new NotFoundException('Could not find attraction.');
+    }
+    return attraction;
   }
 }
