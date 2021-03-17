223301276420e3a62368030e17a13ba9986cb0f8
mateuszbuturla <2002mateuszbuturla@gmail.com>
2021-03-17 21:25:16 +0100

    front-end create footer

---
+++ client/components/layout/Footer/StyledFooter.ts
@@ -0,0 +1,46 @@
+import styled from "styled-components";
+
+const StyledFooter = styled.footer`
+  width: 100vw;
+`;
+
+const StyledSectionsContainer = styled.div`
+  width: 90%;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+`;
+
+const StyledSection = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  padding: 10px 0px;
+  border-bottom: 1px solid #000;
+`;
+
+const StyledSectionHeader = styled.p`
+  font-size: ${(props) => props.theme.fontSize.text.small};
+  font-weight: bold;
+`;
+
+const StyledSectionLink = styled.a`
+  font-size: ${(props) => props.theme.fontSize.text.small};
+  padding: 3px 0px;
+`;
+
+const StyledBottomBar = styled.div`
+  width: 90%;
+  margin: 0 auto;
+  font-size: ${(props) => props.theme.fontSize.text.small};
+  padding: 10px 0px;
+`;
+
+export {
+  StyledFooter,
+  StyledSectionsContainer,
+  StyledSection,
+  StyledSectionHeader,
+  StyledSectionLink,
+  StyledBottomBar,
+};
--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,9 +1,11 @@
 import Theme from "config/styled";
+import { Footer } from "components/layout";
 
 function MyApp({ Component, pageProps }: any) {
   return (
     <Theme>
       <Component {...pageProps} />
+      <Footer />
     </Theme>
   );
 }
