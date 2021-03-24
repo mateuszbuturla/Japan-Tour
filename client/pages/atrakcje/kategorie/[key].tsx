e716517554b44031c6b5c73a676867b098f7f9f7
mateuszbuturla <2002mateuszbuturla@gmail.com>
2021-03-24 15:09:28 +0100

    front-end create attraction from category desktop view

--- client/pages/atrakcje/kategorie/[key].tsx
+++ client/pages/atrakcje/kategorie/[key].tsx
@@ -12,6 +12,7 @@ import {
   Container,
   TilesContainer,
   MostVisitedTilesContainer,
+  TilesWithHeaderContainer,
 } from "components/layout";
 
 export default function Attraction() {
@@ -32,54 +33,62 @@ export default function Attraction() {
           <Header>Zamki</Header>
         </Container>
         <Description />
-        <RegionHeader region="Hokkaido" />
-        <MostVisitedTilesContainer>
-          {Array(4)
-            .fill(0)
-            .map((item: any, index: number) => (
-              <MostVisitedTile
-                img="https://www.telegraph.co.uk/content/dam/Travel/2019/August/iStock-1047662500.jpg"
-                name="Sapporo"
-                description="Lorem ipsum dolor sit amet, consectetur adipiscing elit"
-              />
-            ))}
-        </MostVisitedTilesContainer>
-        <RegionHeader region="Tohoku" />
-        <MostVisitedTilesContainer>
-          {Array(4)
-            .fill(0)
-            .map((item: any, index: number) => (
-              <MostVisitedTile
-                img="https://www.telegraph.co.uk/content/dam/Travel/2019/August/iStock-1047662500.jpg"
-                name="Sapporo"
-                description="Lorem ipsum dolor sit amet, consectetur adipiscing elit"
-              />
-            ))}
-        </MostVisitedTilesContainer>
-        <RegionHeader region="Kanto" />
-        <MostVisitedTilesContainer>
-          {Array(4)
-            .fill(0)
-            .map((item: any, index: number) => (
-              <MostVisitedTile
-                img="https://www.telegraph.co.uk/content/dam/Travel/2019/August/iStock-1047662500.jpg"
-                name="Sapporo"
-                description="Lorem ipsum dolor sit amet, consectetur adipiscing elit"
-              />
-            ))}
-        </MostVisitedTilesContainer>
-        <RegionHeader region="Chubu" />
-        <MostVisitedTilesContainer>
-          {Array(4)
-            .fill(0)
-            .map((item: any, index: number) => (
-              <MostVisitedTile
-                img="https://www.telegraph.co.uk/content/dam/Travel/2019/August/iStock-1047662500.jpg"
-                name="Sapporo"
-                description="Lorem ipsum dolor sit amet, consectetur adipiscing elit"
-              />
-            ))}
-        </MostVisitedTilesContainer>
+        <TilesWithHeaderContainer>
+          <RegionHeader region="Hokkaido" />
+          <MostVisitedTilesContainer>
+            {Array(4)
+              .fill(0)
+              .map((item: any, index: number) => (
+                <MostVisitedTile
+                  img="https://www.telegraph.co.uk/content/dam/Travel/2019/August/iStock-1047662500.jpg"
+                  name="Sapporo"
+                  description="Lorem ipsum dolor sit amet, consectetur adipiscing elit"
+                />
+              ))}
+          </MostVisitedTilesContainer>
+        </TilesWithHeaderContainer>
+        <TilesWithHeaderContainer>
+          <RegionHeader region="Tohoku" />
+          <MostVisitedTilesContainer>
+            {Array(4)
+              .fill(0)
+              .map((item: any, index: number) => (
+                <MostVisitedTile
+                  img="https://www.telegraph.co.uk/content/dam/Travel/2019/August/iStock-1047662500.jpg"
+                  name="Sapporo"
+                  description="Lorem ipsum dolor sit amet, consectetur adipiscing elit"
+                />
+              ))}
+          </MostVisitedTilesContainer>
+        </TilesWithHeaderContainer>
+        <TilesWithHeaderContainer>
+          <RegionHeader region="Kanto" />
+          <MostVisitedTilesContainer>
+            {Array(4)
+              .fill(0)
+              .map((item: any, index: number) => (
+                <MostVisitedTile
+                  img="https://www.telegraph.co.uk/content/dam/Travel/2019/August/iStock-1047662500.jpg"
+                  name="Sapporo"
+                  description="Lorem ipsum dolor sit amet, consectetur adipiscing elit"
+                />
+              ))}
+          </MostVisitedTilesContainer>
+        </TilesWithHeaderContainer>
+        <TilesWithHeaderContainer>
+          <RegionHeader region="Chubu" />
+          <MostVisitedTilesContainer>
+            {Array(4)
+              .fill(0)
+              .map((item: any, index: number) => (
+                <MostVisitedTile
+                  img="https://www.telegraph.co.uk/content/dam/Travel/2019/August/iStock-1047662500.jpg"
+                  name="Sapporo"
+                  description="Lorem ipsum dolor sit amet, consectetur adipiscing elit"
+                />
+              ))}
+          </MostVisitedTilesContainer>
+        </TilesWithHeaderContainer>
       </PageContainer>
     </>
   );
