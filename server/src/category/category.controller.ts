6b670453151c0314a4e3bdea577e90f631dfed49
mateuszbuturla <2002mateuszbuturla@gmail.com>
2021-03-25 11:41:47 +0100

    back-end create category endpoint

--- server/src/category/category.controller.ts
+++ server/src/category/category.controller.ts
@@ -1,6 +1,12 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, UploadedFiles, UseInterceptors, UsePipes } from '@nestjs/common';
+import { FileFieldsInterceptor } from '@nestjs/platform-express';
 import { CategoryInterface } from 'src/interfaces/category';
+import { MulterDiskUploadedFiles } from 'src/interfaces/files';
+import { JoiValidationPipe } from 'src/pipes/JoiValidationPipe';
+import { multerStorage } from 'src/utils/storage';
 import { CategoryService } from './category.service';
+import AddCategoryDto from './dto/AddCategoryDto';
+import { AddCategoryValidator } from './validation/AddCategoryValidator';
 
 @Controller('category')
 export class CategoryController {
@@ -10,6 +16,29 @@ export class CategoryController {
   async getCategories(): Promise<CategoryInterface[]> {
     const categories = await this.categoryService.getCategories();
     return categories;
+  }
+
+  @Post('/')
+  @UsePipes(new JoiValidationPipe(AddCategoryValidator))
+  @UseInterceptors(
+    FileFieldsInterceptor(
+      [
+        {
+          name: 'img',
+          maxCount: 1,
+        },
+      ],
+      {
+        storage: multerStorage(),
+      },
+    ),
+  )
+  async createCategory(
+    @Body() data: AddCategoryDto,
+    @UploadedFiles() img: MulterDiskUploadedFiles,
+  ) {
+    const category = await this.categoryService.createCategory(data, img);
+    return category;
   }
 
   @Get('/:key')
--- server/src/category/category.service.ts
+++ server/src/category/category.service.ts
@@ -1,8 +1,14 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { HttpException, Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { CategoryInterface } from 'src/interfaces/category';
+import { MulterDiskUploadedFiles } from 'src/interfaces/files';
+import NormalizeString from 'src/utils/normalizeString';
+import { storageDir } from 'src/utils/storage';
 import { Category } from './category.entity';
+import AddCategoryDto from './dto/AddCategoryDto';
+import * as fs from 'fs';
+import * as path from 'path';
 
 @Injectable()
 export class CategoryService {
@@ -35,4 +41,42 @@ export class CategoryService {
     }
     return category;
   }
+
+  async createCategory(data: AddCategoryDto, imgs: MulterDiskUploadedFiles) {
+    const img = imgs?.img?.[0] ?? null;
+
+    if (!img) {
+      throw new HttpException('Validation failed', 400);
+    }
+
+    try {
+      const existCategory = await this.categoryModel
+        .find({
+          $or: [{ name: data.name }, { key: NormalizeString(data.name) }],
+        })
+        .exec();
+
+      if (existCategory.length > 0) {
+        throw new HttpException('Category is exist.', 409);
+      } else {
+        const newCategory = new this.categoryModel({
+          name: data.name,
+          key: NormalizeString(data.name),
+          img: img.filename,
+          description: data.description,
+        });
+        const res = await newCategory.save();
+        return res;
+      }
+    } catch (e: any) {
+      try {
+        if (img) {
+          fs.unlinkSync(path.join(storageDir(), img.filename));
+        }
+      } catch (e2) {}
+
+      throw e;
+    }
+  }
+
 }
