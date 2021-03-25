6d72d68a0ecf37d5527abf5800e2feb5e2e34af4
mateuszbuturla <2002mateuszbuturla@gmail.com>
2021-03-25 11:33:04 +0100

    back-end get single category endpoint

--- server/src/category/category.controller.ts
+++ server/src/category/category.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { CategoryInterface } from 'src/interfaces/category';
 import { CategoryService } from './category.service';
 
@@ -6,9 +6,15 @@ import { CategoryService } from './category.service';
 export class CategoryController {
     constructor(private readonly categoryService: CategoryService) {}
 
-    @Get('/')
-    async getCategories(): Promise<CategoryInterface[]> {
-        const categories = await this.categoryService.getCategories();
-        return categories;
-    }
+  @Get('/')
+  async getCategories(): Promise<CategoryInterface[]> {
+    const categories = await this.categoryService.getCategories();
+    return categories;
+  }
+
+  @Get('/:key')
+  async getSingleCity(@Param('key') key: string): Promise<CategoryInterface> {
+    const category = await this.categoryService.getSingleCategory(key);
+    return category;
+  }
 }
--- server/src/category/category.service.ts
+++ server/src/category/category.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { CategoryInterface } from 'src/interfaces/category';
@@ -15,5 +15,24 @@ export class CategoryService {
   async getCategories(): Promise<CategoryInterface[]> {
     const categories = await this.categoryModel.find({});
     return categories;
+  }
+
+  async getSingleCategory(key: string): Promise<CategoryInterface> {
+    const category = await this.findCategory(key);
+
+    return category;
+  }
+
+  private async findCategory(key: string): Promise<CategoryInterface> {
+    let category;
+    try {
+      category = await this.categoryModel.findOne({ key }).exec();
+    } catch (error) {
+      throw new NotFoundException('Could not find category.');
+    }
+    if (!category) {
+      throw new NotFoundException('Could not find category.');
+    }
+    return category;
   }
 }
